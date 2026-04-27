ffd6dafa554b77dfdb0f152418feaf734ab6dfc5
Brzoza5254 <f05212009@gmail.com>
2026-04-27 12:29:30 +0200

    Update network.py

--- network.py
+++ network.py
@@ -1,35 +1,35 @@
 import socket, threading, json, subprocess, re, time
 
 class NetworkNode:
-    def __init__(self, port=5000):
+    def __init__(self, port=5001):
         self.port, self.peers, self.public_url = port, set(), None
         self.start_tunnel()
 
     def start_tunnel(self):
         def run():
-            # Tunelowanie przez localhost.run - mapuje lokalny port na publiczny port 80
-            p = subprocess.Popen(["ssh","-o","StrictHostKeyChecking=accept-new","-R",f"80:localhost:{self.port}","nokey@localhost.run"], stdout=subprocess.PIPE, text=True)
+            # Tunelowanie przez localhost.run
+            p = subprocess.Popen(["ssh","-o","StrictHostKeyChecking=accept-new","-R",f"80:localhost:{self.port}","nokey@localhost.run"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
             for line in p.stdout:
                 m = re.search(r'([\w-]+\.lhr\.life)', line)
                 if m: 
-                    self.public_url = m.group(1)
+                    self.public_url = m.group(1).strip()
                     print(f"[*] Public URL: {self.public_url}")
                     break
         threading.Thread(target=run, daemon=True).start()
 
     def connect_to_peer(self, addr):
-        # Usuwamy ewentualne http:// lub porty, by mieć czysty host
         clean_addr = addr.replace("http://", "").split(":")[0].strip()
-        if clean_addr and clean_addr != self.public_url and clean_addr not in self.peers:
-            self.peers.add(clean_addr)
-            # 1. Przywitaj się i podaj swoich znajomych
-            self.send_to_one(clean_addr, {
-                "type": "GOSSIP_HELLO",
-                "my_url": self.public_url,
-                "known_peers": list(self.peers)
-            })
-            # 2. Poproś o łańcuch do synchronizacji
-            self.send_to_one(clean_addr, {"type": "GET_CHAIN", "my_url": self.public_url})
+        if not clean_addr or clean_addr == self.public_url: return
+        
+        self.peers.add(clean_addr)
+        # 1. Przywitaj się
+        self.send_to_one(clean_addr, {
+            "type": "GOSSIP_HELLO",
+            "my_url": self.public_url,
+            "known_peers": list(self.peers)
+        })
+        # 2. Poproś o łańcuch
+        self.send_to_one(clean_addr, {"type": "GET_CHAIN", "my_url": self.public_url})
 
     def start_server(self, cb):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,44 +40,37 @@ class NetworkNode:
             while True:
                 try:
                     c, _ = s.accept()
-                    d = c.recv(1024*1024)
+                    d = b""
+                    while True:
+                        part = c.recv(4096)
+                        if not part: break
+                        d += part
                     if d:
                         msg = json.loads(d.decode())
-                        # REAKCJA NA GOSSIP: Jeśli poznałeś nowych, połącz się z nimi
                         if msg['type'] == "GOSSIP_HELLO":
-                            new_found = False
-                            for p in msg.get('known_peers', []):
-                                if p != self.public_url and p not in self.peers:
-                                    self.peers.add(p)
-                                    new_found = True
-                                    # Automatyczne łączenie z nowo odkrytymi (REAKCJA ŁAŃCUCHOWA)
-                                    threading.Thread(target=self.connect_to_peer, args=(p,)).start()
-                            
-                            if msg['my_url'] not in self.peers and msg['my_url'] != self.public_url:
+                            if msg['my_url'] and msg['my_url'] != self.public_url and msg['my_url'] not in self.peers:
                                 self.peers.add(msg['my_url'])
-                                # Odpowiedz nowemu peerowi, by on też o Tobie wiedział
-                                self.send_to_one(msg['my_url'], {
-                                    "type": "GOSSIP_HELLO", 
-                                    "my_url": self.public_url, 
-                                    "known_peers": list(self.peers)
-                                })
-                        
+                            for p in msg.get('known_peers', []):
+                                if p and p != self.public_url and p not in self.peers:
+                                    self.peers.add(p)
+                                    # Propagacja sieci
+                                    threading.Thread(target=self.connect_to_peer, args=(p,)).start()
                         cb(msg, self)
                     c.close()
                 except: pass
         threading.Thread(target=run, daemon=True).start()
 
     def send_to_one(self, host, data):
+        if not host: return
         def _send():
             try:
-                # Wymuszamy port 80, bo localhost.run tam odbiera ruch
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(5)
+                s.settimeout(10)
                 s.connect((host.strip(), 80)) 
-                s.send(json.dumps(data).encode())
+                s.sendall(json.dumps(data).encode())
                 s.close()
-            except: pass
-        # Wysyłamy w osobnym wątku, by nie blokować GUI/Minera
+            except Exception as e:
+                print(f"Connection error to {host}: {e}")
         threading.Thread(target=_send).start()
 
     def broadcast(self, data):
